329857847f16f827f519bb29f83420af898ce2aa
Patrick Sheriddan <sheridp2@gmail.com>
2019-01-20 13:55:36 -0800

    frontend scaffolded out, app rendering

--- models/Entry.js
+++ models/Entry.js
@@ -5,7 +5,8 @@ const entrySchema = new Schema({
   body: String,
   highScore: { type: Number, min: 0, max: 100 },
   lowScore: { type: Number, min: 0, max: 100 },
-  entryDate: { type: Date, default: Date.now }
+  entryDate: { type: Date, default: Date.now },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 mongoose.model("entrys", entrySchema);
